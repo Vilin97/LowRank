d04605219ff01e9ab36192306a809556851d9e91
Vasily Ilin <vasilin97@gmail.com>
2020-07-24 17:27:30 -0400

    .

--- test/uniform2d.jl
+++ test/uniform2d.jl
@@ -8,8 +8,8 @@ data_set, (err, indices) = file["data"], file["size4_rank1"]
 k = 1
 n = 4
 
-@test find_low_rank_subset_checkall(data_set,n,k) == (err, indices)
-@test find_low_rank_subset_sample_rep(data_set,n,k)[1] <= 4*err
+@test find_low_rank_subset_checkall(data_set,n,k) <= (err, indices) + 0.0001
+@test find_low_rank_subset_sample_rep(data_set,n,k)[1] <= 2*err + .0001
 @test find_low_rank_subset_iterative(data_set, n, k)[1].error <= err+0.0001
 # PLotting
 # p = scatter([Tuple(x) for x in V], label = "all points")
